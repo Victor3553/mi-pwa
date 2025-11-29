0ce98f0bdf70448e1bc837ff583c02ae2a1ae24b
Victor3553 <22610227@utgz.edu.mx>
2025-11-29 00:05:39 -0600

    Delete 08-Videojuego directory

--- 08-Videojuego/js/anim.js
+++
@@ -1,9 +0,0 @@
-// Cambia la navbar al hacer scroll
-document.addEventListener("scroll", () => {
-    const navbar = document.querySelector("nav");
-    if(window.scrollY > 50){
-        navbar.classList.add("shadow-lg", "bg-dark");
-    } else {
-        navbar.classList.remove("shadow-lg", "bg-dark");
-    }
-});
